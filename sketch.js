1c8cd2ef98057dc7c5a706bd2b80b7efda412db3
jsuraj <jsurajbaramati@gmail.com>
2017-09-17 16:38:28 +0530

    added window.flag to display hello world

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
 function setup() {
 	createCanvas(displayWidth, displayHeight);
+	textSize(20);
+	textAlign(CENTER);
+	window.flag = false;
+}
+
+function draw() {
+	if(window.flag) {
+		text("Hello World", displayWidth/2, displayHeight/2);
+	}
 }
 
 function touchStarted() {
 	background(random(0, 255), random(0, 255), random(0, 255));  
+	// window.flag = !window.flag;
 }
