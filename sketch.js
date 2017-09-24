c5b3fb5a73e6f4666ab8ad753711fa51223e1b40
jsuraj <jsurajbaramati@gmail.com>
2017-09-24 20:14:32 +0530

    sample template

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
 function setup() {
-	createCanvas(displayWidth, displayHeight);
+	createCanvas(windowWidth, windowHeight);							//windowWidth works over displayWidth
+	// console.log(displayWidth+' '+displayHeight);
+	// console.log(windowWidth+' '+windowHeight);
+	textSize(30);
+	textAlign(CENTER);
+	textStyle(BOLD);
+	textFont('Georgia');
+}
+
+function draw() {
+	text("Hello", windowWidth/2, windowHeight/2);
 }
 
 function touchStarted() {
-	background(random(0, 255), random(0, 255), random(0, 255));  
+	background(random(0, 255), random(0, 255), random(0, 255));
 }
